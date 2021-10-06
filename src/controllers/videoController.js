992432a1138e73af27913cb419a96f541371a19b
shwlee <vactormanbear@gmail.com>
2021-10-07 01:09:28 +0900

    add parameter with regex

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,11 @@
 export const starting = (req, res) => res.send("Let's start!");
+
 export const search = (req, res) => res.send("VIDEO SEARCH");
-export const watch = (req, res) => res.send("VIDEO SEEEEEEEEEEEEEEEEEEE!");
-export const remove = (req, res) => res.send("VIDEO DELETE!");
-export const edit = (req, res) => res.send("VIDEO EDIT!");
+
+export const watch = (req, res) => res.send(`VIDEO SEEEEEEEEEEEEEEEEEEE!Now you watching ${req.params.id}`);
+
+export const edit = (req, res) => res.send(`VIDEO EDIT! this video id is ${req.params.id}`);
+
+export const remove = (req, res) => res.send(`VIDEO DELETE! Do you really delete ${req.params.id} video`);
+
 export const upload = (req, res) => res.send("VIDEO UPLOAD!");
