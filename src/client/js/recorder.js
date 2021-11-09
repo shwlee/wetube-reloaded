161347e915aaf52cdbc7ac4915c4a6a3b0f2bb55
shwlee <sh.lee@cenacle.com>
2021-11-09 22:58:48 +0900

    add recorder;

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,10 +1,73 @@
 import "./main.js";
 
-const recorder = document.getElementById("recorder");
+const video = document.getElementById("recorder");
+const recorderButton = document.getElementById("recordButton");
 
-const recordUserVideoHandler = async () => {
-    const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
-    console.log(stream);
+let toggleOn;
+let mediaStream;
+let mediaRecorder;
+
+const recordingOption = {
+    mimeType: "video/webm"
 }
 
-recorder.addEventListener("click", recordUserVideoHandler);
+const init = async () => {
+    toggleOn = false;
+    recorderButton.innerText = "Pause";
+    video.volume = 0;
+    mediaStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+    if (mediaStream) {
+        console.log(mediaStream);
+        video.srcObject = mediaStream;
+        video.play();
+    }
+}
+
+const handleDataAvailable = (event) => {
+    console.log("data-available");
+
+    if (event.data.size > 0) {
+
+        console.log(event.data);
+        download(event.data);
+    } else {
+
+    }
+}
+
+const download = (data) => {
+
+    const url = URL.createObjectURL(data);
+
+    const a = document.createElement("a");
+    a.style = "display: none";
+    a.href = url;
+    a.download = "record.webm";
+    document.body.appendChild(a);
+
+    a.click();
+
+    window.URL.revokeObjectURL(url);
+}
+
+const recordUserVideoHandler = async () => {
+    if (!video) {
+        return;
+    }
+
+    toggleOn = !toggleOn;
+    recorderButton.innerText = toggleOn ? "Pause" : "Recording";
+
+    if (!mediaRecorder) {
+        mediaRecorder = new MediaRecorder(mediaStream, recordingOption);
+        mediaRecorder.ondataavailable = handleDataAvailable;
+    }
+
+    const action = toggleOn ? () => mediaRecorder.start() : () => mediaRecorder.stop();
+
+    action();
+}
+
+init();
+
+recorderButton.addEventListener("click", recordUserVideoHandler);
